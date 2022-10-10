01689a8fdedfd7c6221fb3d08ed3a6b002bd55a2
Gracelyn Prajna <gracelynprajna@gmail.com>
2022-10-09 22:25:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ alert("Hello " + playerName + "!")
 
 let playerHealth = 15;
 
-let monsterName = "Snorlax";
+let monsterName = 'Snorlax';
 
 let monsterHealth = 15;
 
@@ -12,61 +12,53 @@ function randomNum(min, max) {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min) + min) 
   }
-  
-let playerAttackPoints = randomNum(1,6);
+
 
 function playerAttack(){
-    let playerAttackPoints = randomNum(1,6);
-    monsterHealth = monsterHealth - playerAttackPoints
-    alert(`Player attacked monster and did ${playerAttackPoints} damage! Monster has ${monsterHealth} health left!`) 
+let playerAttackPoints = randomNum(1,6);
+monsterHealth = monsterHealth - playerAttackPoints;
+alert(`Player attacked monster and did ${playerAttackPoints} damage! Monster has ${monsterHealth} health left!`) 
 }
-
 
 
 function monsterAttack(){
-    let monsterAttackPoints = randomNum(1,6);
-    playerHealth = playerHealth - monsterAttackPoints
-    alert(`Monster attacked player and did did ${monsterAttackPoints} damage! ${playerName} has ${playerHealth} health left!`)
+let monsterAttackPoints = randomNum(1,6);
+playerHealth = playerHealth - monsterAttackPoints;
+alert(`Monster attacked player and did did ${monsterAttackPoints} damage! ${playerName} has ${playerHealth} health left!`)
 }
 
-
-
-function playRound() {
-  randomNum(0,2);
-  if (randomNum()===0){
-    alert(`${playerName}, make the first move.`);
-    playerAttack();
+function playRound(){
+    randomNum(0,2);
+    if (randomNum(0,2)===0){
+        playerAttack();
     if (monsterHealth>0){
       monsterAttack()
     }
   }
-  else if (randomNum()===1){
-    alert(`${monsterName} makes the first move.`);
-    monsterAttack()
+  else if (randomNum(0,2)===1){
+    monsterAttack();
     if (playerHealth>0){
-      playerAttack()
+      playerAttack();
     }
   }
 }
 
-  function playGame(){
-    alert(`Hello, ${playerName}! You are fighting ${monsterName}! 
-    Your health is ${playerHealth}, ${monsterName}'s health is ${monsterHealth}`);
-    let roundNumber = 0
+function playGame(){
+    alert(`Hello ${playerName}! You are fighting ${monsterName}! 
+    ${playerName} health is ${playerHealth}, and ${monsterName}'s 
+    health is ${monsterHealth}!`);
+    let roundNumber = 0;
         while(playerHealth<=0 || monsterHealth<=0){
-        playRound();
         roundNumber++;
         alert(`this is round number ${roundNumber}. 
-        ${playerName} is at ${playerHealth} health and ${monsterName} is at ${monsterHealth} health.`); 
+        ${playerName} is at ${playerHealth} health and ${monsterName} is at ${monsterHealth} health.`)
+        playRound(); 
     }
-  }
-
-
 if (playerHealth === 0) {
     alert(`${monsterName} wins! You lose.`);
   }
   else if (monsterHealth === 0) {
     alert(`${playerName} wins!!! Congratulations!!`)
   }
-
+  }
   playGame(); 
