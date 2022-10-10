5cd6d0a76c22b873fd0920ce29adcd83e477554c
Gracelyn Prajna <gracelynprajna@gmail.com>
2022-10-09 22:28:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -48,16 +48,16 @@ function playGame(){
     ${playerName} health is ${playerHealth}, and ${monsterName}'s 
     health is ${monsterHealth}!`);
     let roundNumber = 0;
-        while(playerHealth<=0 || monsterHealth<=0){
+        while(playerHealth>=0 || monsterHealth>=0){
         roundNumber++;
         alert(`this is round number ${roundNumber}. 
         ${playerName} is at ${playerHealth} health and ${monsterName} is at ${monsterHealth} health.`)
         playRound(); 
     }
-if (playerHealth === 0) {
+if (playerHealth <= 0) {
     alert(`${monsterName} wins! You lose.`);
   }
-  else if (monsterHealth === 0) {
+  else if (monsterHealth <= 0) {
     alert(`${playerName} wins!!! Congratulations!!`)
   }
   }
