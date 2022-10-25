992af13e2dcbb40d206a358a477f7d1213f0b274
Gracelyn Prajna <gracelynprajna@gmail.com>
2022-10-25 01:26:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-//comment
-
 let playerName = prompt("Enter your name.")
 alert("Hello " + playerName + "!")
 
-let playerHealth = 15;
+class Fighter{
+  constructor(name){
+    this.name = name;
+    this.healthPoints = healthPoints;
+    this.attackMethod = attackMethod;
+  }
+  attack(Fighter){
+    let attackPoints = randomNum(1,6);
+    Fighter.healthPoints = Fighter.healthPoints - attackPoints;
+    alert(`${this.name} attacked ${Fighter.name} and did did ${attackPoints} damage! ${Fighter.name} has ${Fighter.healthPoints} health left!`)
+  }
+}
 
-let monsterName = 'Snorlax';
+class Monster extends Fighter{
+  constructor(name){
+    super(name,healthPoints)
+    this.healthPoints = 15;
+    this.attackMethod = "kick"
+  }
+}
 
-let monsterHealth = 15;
+let Monster = new Monster("Snorlax")
+
+class Hero extends Fighter{
+  constructor(name){
+    super(name,healthPoints)
+    this.attackMethod = "punch";
+  }
+}
+
+let Hero = new Hero (playerName)
+
 
 function randomNum(min, max) {
     min = Math.ceil(min)
@@ -16,50 +41,51 @@ function randomNum(min, max) {
   }
 
 
-function playerAttack(){
-let playerAttackPoints = randomNum(1,6);
-monsterHealth = monsterHealth - playerAttackPoints;
-alert(`Player attacked monster and did ${playerAttackPoints} damage! Monster has ${monsterHealth} health left!`) 
+/*function playerAttack(){
+let attackPoints = randomNum(1,6);
+monsterHealth = monsterHealth - attackPoints;
+alert(`Player attacked monster and did ${attackPoints} damage! Monster has ${monsterHealth} health left!`) 
 }
 
 
 function monsterAttack(){
-let monsterAttackPoints = randomNum(1,6);
-playerHealth = playerHealth - monsterAttackPoints;
-alert(`Monster attacked player and did did ${monsterAttackPoints} damage! ${playerName} has ${playerHealth} health left!`)
+let attackPoints = randomNum(1,6);
+playerHealth = playerHealth - attackPoints;
+alert(`Monster attacked player and did did ${attackPoints} damage! ${playerName} has ${playerHealth} health left!`)
 }
+*/
 
 function playRound(){
     randomNum(0,2);
     if (randomNum(0,2)===0){
-        playerAttack();
-    if (monsterHealth>0){
-      monsterAttack()
+        Hero.attack(Monster);
+    if (Monster.healthPoints>0){
+      Monster.attack(Hero)
     }
   }
   else if (randomNum(0,2)===1){
-    monsterAttack();
-    if (playerHealth>0){
-      playerAttack();
+    Monster.attack(Hero);
+    if (Hero.healthPoints>0){
+      Hero.attack(Monster);
     }
   }
 }
 
 function playGame(){
-    alert(`Hello ${playerName}! You are fighting ${monsterName}! 
-    ${playerName}'s health is ${playerHealth}, and ${monsterName}'s health is ${monsterHealth}!`);
+    alert(`Hello ${Hero.name}! You are fighting ${Monster.name}! 
+    ${Hero.name}'s health is ${Hero.healthPoints}, and ${Monster.name}'s health is ${Monster.healthPoints}!`);
     let roundNumber = 0;
-        while(playerHealth>0 && monsterHealth>0){
+        while(Hero.healthPoints>0 && Monster.healthPoints>0){
         roundNumber++;
         alert(`this is round number ${roundNumber}. 
-        ${playerName} is at ${playerHealth} health and ${monsterName} is at ${monsterHealth} health.`)
+        ${Hero.name} is at ${Hero.healthPoints} health and ${Monster.name} is at ${Monster.healthPoints} health.`)
         playRound(); 
         }
-        if (playerHealth <= 0) {
-          alert(`${monsterName} wins! You lose.`);
+        if (Hero.healthPoints <= 0) {
+          alert(`${Monster.name} wins! You lose.`);
         }
-        else if (monsterHealth <= 0) {
-    alert(`${playerName} wins!!! Congratulations!!`)
+        else if (Monster.healthPoints <= 0) {
+    alert(`${Hero.name} wins!!! Congratulations!!`)
     }
   }
   playGame(); 
